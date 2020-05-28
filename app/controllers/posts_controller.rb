275f3d403f683a112b443f6b547a5340ccbae47d
fuminyan-238 <fuminyan238@gmail.com>
2020-05-28 14:41:13 +0900

    add_view_edit
    投稿編集画面のフロントとコントローラーを記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,19 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    if post.save
+      redirect_to root_path, notice:"投稿が完了しました"
+    else
+      render :edit
+    end
+  end
 
   private
   def post_params
