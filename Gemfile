e028f2d422f10e87e3223c5db779569c79d3666b
fuminyan-238 <fuminyan238@gmail.com>
2020-05-28 17:31:04 +0900

    fix
    自動デプロイのための処理

--- Gemfile
+++ Gemfile
@@ -64,3 +64,7 @@ gem 'carrierwave'
 gem 'mini_magick'
 gem 'pry-rails'
 gem 'kaminari'
+
+group :production do
+  gem 'unicorn'
+end
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.14.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "design-island"
+set :repo_url, "git@example.com:fuminyan-238/design-island.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
