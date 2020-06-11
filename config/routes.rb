0e3cf37cc4990992f7e3b83f7527770d57120119
fuminyan-238 <fuminyan238@gmail.com>
2020-06-11 21:57:20 +0900

    fix
    検索機能のルーティング周りの変更（まだ途中）

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,12 @@ Rails.application.routes.draw do
 
   resources :users, only: [:edit, :update]
   resources :posts do
-    resources :comments, only: :create
     collection do
       get "search"
-      # post "/posts/search" => "posts#search"
+      # post "/" => "posts#search"
     end
+    resources :comments, only: :create
+    
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
