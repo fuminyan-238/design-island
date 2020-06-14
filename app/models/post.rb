b34f4107447ce6fedaea286e1c88d22f5e79cb4e
fuminyan-238 <fuminyan238@gmail.com>
2020-06-14 19:53:33 +0900

    add_like_contoroller
    likeコントローラーの追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
   has_many :comments
+  has_many :liked_users, through: :likes, source: :user
   mount_uploader :image, ImageUploader
 
   def self.search(search)
